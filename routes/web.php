902a51671733bad035fb5fc7edba99a900a7791a
ayuannisa17 <aryaniannisa4@gmail.com>
2026-02-21 22:03:52 +0700

    Praktikum 6 - Implementasi Eloquent ORM

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,9 @@
 <?php
 
 use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\Levelcontroller;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,3 +11,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
