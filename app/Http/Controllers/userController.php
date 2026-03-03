3b8d79a4909a3544d65c23f8bd1a62a2a43d0cdd
ayuannisa17 <aryaniannisa4@gmail.com>
2026-03-03 22:17:28 +0700

    praktikum2.2

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -10,10 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::find0r(1, ['username', 'nama'], function () { 
-            abort(404);
-        });
-        
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
